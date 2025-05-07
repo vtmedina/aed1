2a7f673261c9cff61223c505d11f54e2c1b429b4
vtmedina <vtmedina@inf.ufpel.edu.br>
2025-05-07 20:00:32 -0300

    Add files via upload

---
+++ programação descomplicada/lista1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int numero;
+    
+    printf("número inteiro: ");
+    scanf("%d", &numero);
+    
+    printf("o número digitado foi: %d\n", numero);
+    
+    return 0;
+}
