dd71e5fb15c03650a5bc4a93be2433c2efe0e160
vtmedina <vtmedina@inf.ufpel.edu.br>
2025-06-29 18:43:30 -0300

    Update 1405.c

--- leetcode/1405/1405.c
+++ leetcode/1405/1405.c
@@ -1,14 +1,55 @@
-def longestDiverseString(a: int, b: int, c: int) -> str:
-    from heapq import heappush, heappop
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-    heap = []
-    if a > 0:
-        heappush(heap, (-a, 'a'))
-    if b > 0:
-        heappush(heap, (-b, 'b'))
-    if c > 0:
-        heappush(heap, (-c, 'c'))
+// estrutura para guardar quantas vezes ainda podemos usar a letra e qual é o caractere
+typedef struct {
+    int count;
+    char ch;
+} Item;
 
-    result = []
+// função para ordenar um vetor de tamanho n de forma decrescente pelo count, o caractere com maior quantidade fica na frente
+// essa ordenação simula o comportamento de uma max-heap
+void sort(Item *arr, int n) {
+    for(int i = 0; i < n-1; i++)
+        for(int j = i+1; j < n; j++)
+            if(arr[i].count < arr[j].count) {
+                Item temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+}
 
-\\ nao consegui terminar em aula
+char* longestDiverseString(int a, int b, int c) {
+    Item items[3] = {{a, 'a'}, {b, 'b'}, {c, 'c'}}; // vetor com cada caractere e quantidade restante
+    char *result = (char*)malloc(200 + 1); // buffer para a string com o resultado
+    int len = 0;
+
+    result[0] = '\0';
+
+    while (1) {
+        sort(items, 3); \\ garantindo que o caractere com mais quantidade restante esteja em items[0].
+        int appended = 0;
+
+        // Percorre os 3 itens, do maior para o menor, guarda o tamanho atual da string, se a quantidade daquela letra acabou, pula para o próximo.
+        // Se os dois últimos caracteres da string já forem iguais ao caractere que queremos adicionar, pulamos para o próximo.
+        for (int i = 0; i < 3; i++) {
+            int l = len;
+            if (items[i].count == 0) continue;
+            if (len >= 2 && result[len-1] == items[i].ch && result[len-2] == items[i].ch)
+                continue;
+          
+        // Adiciona o caractere escolhido na string e incrementa seu tamanho, atualiza o terminador nulo para manter a string válida.
+        // Decrementa a quantidade restante desse caractere, pois ele ja foi usado e marca que adicionamos algum caractere nessa rodada do loop.
+            result[len++] = items[i].ch;
+            result[len] = '\0';
+            items[i].count--;
+            appended = 1;
+            break;
+        }
+
+        if (!appended) break; // nenhum caractere foi adicionado porque violou a regra
+    }
+
+    return result;
+}
